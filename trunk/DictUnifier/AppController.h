19372882194b5f82168bfa488b221bad0ec98dbe
gzjjgod <gzjjgod@a74d8ee0-1c41-0410-92f9-01bff1c2377c>
2008-03-31 12:58:41 +0000

    Upgrade to 1.1.0

--- trunk/DictUnifier/AppController.h
+++ trunk/DictUnifier/AppController.h
@@ -3,6 +3,7 @@
 
 @interface AppController : NSObject {
     IBOutlet NSWindow    *window;
+    IBOutlet NSButton    *chooseDictionaryButton;
     IBOutlet NSTextField *dictionaryIDField;
     IBOutlet NSTextField *dictionaryNameField;
     IBOutlet NSTextField *dictionaryWordCountField;
@@ -10,11 +11,15 @@
     IBOutlet NSPathControl *dictionaryPath;
     IBOutlet NSProgressIndicator *progressIndicator;
     IBOutlet NSButton    *convertButton;
+    IBOutlet NSButton    *useScriptButton;
+    IBOutlet NSButton    *chooseScriptButton;
+    IBOutlet NSPathControl *scriptFilePath;
     BOOL                  windowExpanded;
     SDDictionary         *dictionary;
     NSFileHandle         *readHandle;
 }
 - (IBAction)chooseFile:(id)sender;
+- (IBAction)useScript:(id)sender;
 - (IBAction)convertDictionary:(id)sender;
 - (IBAction)expandWindow:(id)sender;
 @end
